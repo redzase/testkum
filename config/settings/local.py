27ddd5768dfa2efe21b0e877d36a8f1df3913815
Muhammad Redza Syahrial Effendi <>
2018-01-30 20:35:28 +0700

    db

--- config/settings/local.py
+++ config/settings/local.py
@@ -36,4 +36,4 @@ DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TEMPLATE_CONTEXT': True,
 }
 
-ALLOWED_HOSTS = ['localhost', 'https://test-kum.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'test-kum.herokuapp.com']
